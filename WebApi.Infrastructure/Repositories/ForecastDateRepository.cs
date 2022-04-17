e5b7cb65962127fce28443374c15b235ea1bd99d
Ermilyev <a.ermilyev@gmail.com>
2022-04-17 09:50:26 +0300

    init web-api infrastructure
    add web-api infrastructure project files

---
+++ WebApi.Infrastructure/Repositories/ForecastDateRepository.cs
@@ -0,0 +1,33 @@
+﻿using Common.Infrastructure.Repositories;
+using Common.Infrastructure.Utility.EF;
+using Common.Models.Models;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Domain.Repositories;
+
+namespace WebApi.Infrastructure.Repositories;
+
+public class ForecastDateRepository : CRUDRepository<ForecastDate>, IForecastDateRepository
+{
+    public ForecastDateRepository(EFContext context) : base(context) { }
+    
+    public async Task<IReadOnlyList<ForecastDate>> GetByFilterAsync(Guid[]? ids = null, DateTime[]? dates = null)
+    {
+        var entities = await GetAllAsync();
+
+        if (ids is not null)
+            entities = entities.Where(c => ids.Contains(c.Id)).ToList();
+        
+        if (dates is not null)
+            entities = entities.Where(c => dates.Contains(c.Date)).ToList();
+
+        return entities;
+    }
+
+    public async Task<ForecastDate?> GetForecastDateByDateAsync(DateTime date)
+    {
+        return (await Context.Set<ForecastDate>()
+            .AsQueryable()
+            .Where(x => x.Date == date)
+            .FirstOrDefaultAsync());
+    }
+}
---
+++ WebApi.Infrastructure/Repositories/WeatherRepository.cs
@@ -0,0 +1,37 @@
+﻿using Common.Infrastructure.Repositories;
+using Common.Infrastructure.Utility.EF;
+using Common.Models.Models;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Domain.Repositories;
+
+namespace WebApi.Infrastructure.Repositories;
+
+public class WeatherRepository : CRUDRepository<Weather>, IWeatherRepository
+{
+    public WeatherRepository(EFContext context) : base(context) {}
+
+    public async Task<IReadOnlyList<Weather>> GetByFilterAsync(Guid[]? ids = null, Guid[]? cityIds = null,
+        Guid[]? forecastDateIds = null)
+    {
+        var entities = await GetAllAsync();
+
+        if (ids is not null)
+            entities = entities.Where(item => ids.Contains(item.Id)).ToList();
+
+        if (cityIds is not null)
+            entities = entities.Where(item => cityIds.Contains(item.CityId)).ToList();
+
+        if (forecastDateIds is not null)
+            entities = entities.Where(item => forecastDateIds.Contains(item.ForecastDateId)).ToList();
+
+        return entities.ToArray();
+    }
+
+    public async Task<Weather?> GetWeatherAsync(Guid cityId, Guid forecastDateId)
+    {
+        return (await Context.Set<Weather>()
+            .AsQueryable()
+            .Where(x => (x.CityId == cityId) && (x.ForecastDateId == forecastDateId))
+            .FirstOrDefaultAsync());
+    }
+}
