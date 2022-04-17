5ff90f5bc923f81810b9d0ec0bda085eb3c247ef
Ermilyev <a.ermilyev@gmail.com>
2022-04-17 09:58:48 +0300

    init client infrastructure
    add client infrastructure project files

---
+++ Client.Infrastructure/Repositories/Store/WeatherRepository.cs
@@ -0,0 +1,32 @@
+﻿using Client.Domain.Repositories.Store;
+using Client.Models.Weather;
+using Common.Infrastructure.Utility.RestSharp.Store;
+using Microsoft.Extensions.Configuration;
+
+namespace Client.Infrastructure.Repositories.Store;
+
+public class WeatherRepository : IWeatherRepository
+{
+    private readonly Uri _api;
+    private readonly IRestRepository _restRepository;
+
+    public WeatherRepository(IConfiguration configuration,
+                             IRestRepository restRepository)
+    {
+        _api = new Uri(string.Concat(configuration["API:Url"],configuration["API:Version"]));
+        _restRepository = restRepository;
+    }
+
+    public async Task<WeatherSummary> GetByFilter(int skip, int? limit, Guid[]? ids = null, Guid[]? cityIds = null,
+                                                  Guid[]? forecastDateIds = null)
+    {
+        var relativeUri = UrlAdaptor.GetWeatherLink(skip, limit, ids, cityIds, forecastDateIds);
+        return await _restRepository.Get<WeatherSummary>(new Uri(_api, relativeUri));
+    }
+
+    public async Task<WeatherModel> GetWeather(Guid id)
+    {
+        var relativeUri = $"weather/{id}";
+        return await _restRepository.Get<WeatherModel>(new Uri(_api, relativeUri));
+    }
+}
