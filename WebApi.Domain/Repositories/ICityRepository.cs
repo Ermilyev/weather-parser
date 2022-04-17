71328e7fa66e0c541b7646a30b5544d6905999c4
Ermilyev <a.ermilyev@gmail.com>
2022-04-17 09:49:42 +0300

    init web-api domain
    add web-api domain project files

---
+++ WebApi.Domain/Repositories/ICityRepository.cs
@@ -0,0 +1,10 @@
+﻿using Common.Domain.Repositories;
+using Common.Models.Models;
+
+namespace WebApi.Domain.Repositories;
+
+public interface ICityRepository : ICRUDRepository<City>
+{
+    Task<IReadOnlyList<City>> GetByFilterAsync(Guid[]? ids = null, string[]? names = null);
+    Task<City?> GetCityByNameAsync(string name);
+}
---
+++ WebApi.Domain/Repositories/IForecastDateRepository.cs
@@ -0,0 +1,10 @@
+﻿using Common.Domain.Repositories;
+using Common.Models.Models;
+
+namespace WebApi.Domain.Repositories;
+
+public interface IForecastDateRepository : ICRUDRepository<ForecastDate>
+{
+    Task<IReadOnlyList<ForecastDate>> GetByFilterAsync(Guid[]? ids = null, DateTime[]? dates = null);
+    Task<ForecastDate?> GetForecastDateByDateAsync(DateTime date);
+}
